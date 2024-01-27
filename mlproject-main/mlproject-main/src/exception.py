72f64efe47672100b5bb0eeeb4734a81830b94ce
Abhinaba Banerjee <1508@DOMAIN.APCFSS.IN>
2024-01-27 13:53:53 +0530

    EDA and Model Traning

--- mlproject-main/mlproject-main/src/exception.py
+++ mlproject-main/mlproject-main/src/exception.py
@@ -9,6 +9,8 @@ def error_message_detail(error,error_detail:sys):
 
     return error_message
 
+    
+
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
@@ -17,11 +19,6 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-'''
-if __name__ == "__main__":
-    try:
-        a= 1/0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e,sys) 
-'''
+
+
+        
--- mlproject-main/mlproject-main/src/logger.py
+++ mlproject-main/mlproject-main/src/logger.py
@@ -15,6 +15,3 @@ logging.basicConfig(
 
 
 )
-
-if __name__ == "__main__":
-    logging.info("Logging has started")
