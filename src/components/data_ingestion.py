52fe6300fc3f90339d82fb9469bdfbec9ab99f04
Abhinaba Banerjee <1508@DOMAIN.APCFSS.IN>
2024-01-31 23:01:36 +0530

    data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,14 +10,15 @@ from src.logger import logging
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-'''
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
-#
 
+'''
 from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
 '''
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -58,10 +59,10 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
-'''
+
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
-
+'''
     modeltrainer=ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 '''
