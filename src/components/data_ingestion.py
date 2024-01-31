c2f32f78471bb890af17e6e5771dfce5daec28e1
Abhinaba Banerjee <1508@DOMAIN.APCFSS.IN>
2024-01-31 23:37:31 +0530

    Model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -14,10 +14,10 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-'''
+
 from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
-'''
+
 
 @dataclass
 class DataIngestionConfig:
@@ -62,8 +62,8 @@ if __name__=="__main__":
 
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
-'''
+
     modeltrainer=ModelTrainer()
     print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
-'''
+
 
